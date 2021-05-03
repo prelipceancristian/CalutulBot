390b7b5cf281c016dcd4e8ed2f610745c0c24d0c
Prelipcean Cristian <prelipceancristian100@gmail.com>
2021-05-03 21:00:20 +0300

    sell() functionality

--- modules/Shopkeeper/Shopkeeper.js
+++ modules/Shopkeeper/Shopkeeper.js
@@ -21,20 +21,48 @@ class Shopkeeper {
         return this.shopRepo.getAll();
     }
 
+    /**
+     * The function handles the buying operation
+     * - creates an account if needed
+     * - checks that the user does not have the item and enough money
+     * - updates the bankAccount
+     * - adds the item to the inventory
+     * - log the transaction
+     * @param {string} userId - the id of the user 
+     * @param {string} itemId - the id of the item
+     */
     async buy(userId, itemId){
         const userBankAccount = await this.bankRepo.read(userId);
         const boughtItem = await this.shopRepo.read(itemId);
         if(!(await this.inventory.hasAcc(userId)))
-            await this.inventory.create(userId);
-        if(await this.inventory.hasItem(userId, itemId))
+            await this.inventory.create(userId);//create an inventory just in case
+
+        if(await this.inventory.hasItem(userId, itemId)) //check first if the user already has the item
             throw new ShopkeeperError('You already have this item!');
-        if(userBankAccount.amount < boughtItem.price)
+        
+        if(userBankAccount.amount < boughtItem.price) // if not, make sure that the user has enough money
             throw new ShopkeeperError('You do not have enough money for this item!');
-        const newUserBankAccount = new BankAccount(userId, userBankAccount.amount - boughtItem.price);
-        await this.inventory.add(userId, itemId);
-        await this.bankRepo.update(newUserBankAccount);
-        //console.log(await this.shopRepo.read(itemId));
-        await this.transactionLogger.logToFile(userId, await this.shopRepo.read(itemId), true);
+        
+        const newUserBankAccount = new BankAccount(userId, userBankAccount.amount - boughtItem.price); // the bank account after paying for the item
+        await this.inventory.add(userId, itemId); // add the item to the inventory of the user
+        await this.bankRepo.update(newUserBankAccount); // update the new tem account
+        await this.transactionLogger.logToFile(userId, await this.shopRepo.read(itemId), true); // log the transaction
+    }
+
+    async sell(userId, itemId){
+        const sellIndex = 0.5;
+        const userBankAccount = await this.bankRepo.read(userId);
+        const soldItem = await this.shopRepo.read(itemId);
+        if(!(await this.inventory.hasAcc(userId)))
+            await this.inventory.create(userId);//create an inventory just in case
+
+        if(!(await this.inventory.hasItem(userId, itemId))) //check first if the user already has the item
+            throw new ShopkeeperError('You do not have this item!');
+        
+        const newUserBankAccount = new BankAccount(userId, userBankAccount.amount + sellIndex * soldItem.price); // the bank account after selling the item
+        await this.inventory.remove(userId, itemId); // add the item to the inventory of the user
+        await this.bankRepo.update(newUserBankAccount); // update the new tem account
+        await this.transactionLogger.logToFile(userId, await this.shopRepo.read(itemId), false); // log the transaction
     }
 }
 
--- test/testShopkeeper.js
+++ test/testShopkeeper.js
@@ -8,6 +8,7 @@ const SoundBite = require('../modules/Shop/SoundBite');
 const fs = require('fs').promises;
 const assert = require('assert');
 const BankAccount = require('../modules/CalutulBank/Domain/BankAccount');
+const { setgroups } = require('process');
 const filePath1 = 'test/AuxFiles/testFile.json';
 const filePath2 = 'test/AuxFiles/testFile2.json';
 const filePath3 = 'test/AuxFiles/testFile3.json';
@@ -62,6 +63,9 @@ describe('Shopkeeper', function(){
             await setup();
             
             await shopkeeper.buy(bk1.UId, sh1.id);
+            var res = await bankRepo.read(bk1.UId);
+            assert.strictEqual(res.amount, 900);
+
             try {
                 await shopkeeper.buy(bk1.UId, sh2.id);
                 assert.fail();
@@ -77,6 +81,68 @@ describe('Shopkeeper', function(){
                 if(error.name != 'ShopkeeperError')
                     assert.fail();
             }
-        })
+            var newBankAcc1 = new BankAccount(bk1.UId, 1001);
+            await bankRepo.update(newBankAcc1);
+            await shopkeeper.buy(bk1.UId, sb1.id);
+            var res = await bankRepo.read(newBankAcc1.UId);
+            assert.strictEqual(res.amount, 1);
+        });
+        it('Buying the same item multiple times', async function(){
+            await clearFiles();
+
+            await setup();
+
+            await shopkeeper.buy(bk1.UId, sh1.id);
+            try {
+                await shopkeeper.buy(bk1.UId, sh1.id);
+                assert.fail();
+            } catch (error) {
+                if(error.name != 'ShopkeeperError')
+                    assert.fail();
+            }
+            try {
+                await shopkeeper.buy(bk1.UId, sh1.id);
+                assert.fail();
+            } catch (error) {
+                if(error.name != 'ShopkeeperError')
+                    assert.fail();
+            }
+            await inventory.remove(bk1.UId, sh1.id);
+            await shopkeeper.buy(bk1.UId, sh1.id);
+        });
     });
+    describe('sell()', function(){
+        it('Default usage', async function(){
+            await clearFiles();
+
+            await setup();
+
+            await shopkeeper.buy(bk1.UId, sb1.id);
+            var res = await bankRepo.read(bk1.UId);
+            assert.deepStrictEqual(res.amount, 0);
+            assert.strictEqual(await inventory.hasItem(bk1.UId, sb1.id), true);
+
+            await shopkeeper.sell(bk1.UId, sb1.id);
+            res = await bankRepo.read(bk1.UId);
+            assert.deepStrictEqual(res.amount, 0.5 * bk1.amount);
+            assert.strictEqual(await inventory.hasItem(bk1.UId, sb1.id), false);
+        });
+        it('Selling inexistent items', async function(){
+            await clearFiles();
+
+            await setup();
+
+            await shopkeeper.buy(bk1.UId, sh1.id);
+            try {
+                await shopkeeper.sell(bk1.UId, sb2.id);
+                assert.fail();
+            } catch (error) {
+                if(error.name != 'ShopkeeperError')
+                    assert.fail();
+            }
+            await shopkeeper.buy(bk1.UId, sb2.id);
+            await shopkeeper.sell(bk1.UId, sb2.id);
+
+        })
+    })
 });
